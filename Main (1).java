d5f9e75431c1128e499c6d26bec34f069b17b7d9
Code-Riffler <145957551+Code-Riffler@users.noreply.github.com>
2024-03-22 11:26:56 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args){
+	    Scanner sc=new Scanner(System.in);
+	    
+	    System.out.println("Enter n number");
+	    int n=sc.nextInt();
+	    
+	    for(n=0;n<=10;n++)
+	    {
+	        System.out.println(2*n);
+	    }
+	}
+}
