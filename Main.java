29584883acc09b131abb8eb91cb8dffdfd327164
Code-Riffler <145957551+Code-Riffler@users.noreply.github.com>
2024-03-26 11:20:34 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,32 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
 import java.util.Scanner;
-
 public class Main
-
 {
-	
-	public static void main(String[] args)
-	{
-	    
+	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-	    
-	    
-		System.out.println("Enter number 1");
-	    
+		System.out.println("Enter number-1");
 		int a=sc.nextInt();
-	    
-	    
-		System.out.println("Enter number 2");
-	    
+		
+		System.out.println("Enter number-2");
 		int b=sc.nextInt();
-	    
-	    
-		System.out.println("Enter number 3");
-	    
-		int c=sc.nextInt();
-	    
-	    
-		System.out.println("a=" +a+ "b=" +b+ "c=" +c);
-	
+		
+		int c=Math.max(a,b);
+		System.out.println("The max value is "+c);
 	}
-
 }
